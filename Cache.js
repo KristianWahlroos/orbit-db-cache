a87f841bec0517bb9ea8fddddbe5b1ec6e72e50c
Mark Henderson <henderson.mark@gmail.com>
2019-05-16 14:32:04 -0400

    persistent store updates

--- Cache.js
+++ Cache.js
@@ -9,73 +9,11 @@ Logger.setLogLevel('ERROR')
 let caches = {}
 
 class Cache {
-  constructor (storage, directory) {
-    this.path = directory || './orbitdb'
-    this._storage = storage
-    this._store = null
-  }
-
-  // Setup storage backend
-  async open () {
-    logger.debug('open', this.path)
-
-    if (this.store) {
-      return Promise.resolve()
-    }
-
-    return new Promise((resolve, reject) => {
-      const store = this._storage(this.path)
-      store.open((err) => {
-        if (err) {
-          return reject(err)
-        }
-        this._store = store
-        resolve()
-      })
-    })
-  }
-
-  async close () {
-    logger.debug('close', this.path)
-
-    if (!this._store) {
-      return Promise.resolve()
-    }
-
-    return new Promise((resolve, reject) => {
-      this._store.close((err) => {
-        if (err) {
-          return reject(err)
-        }
-        this._store = null
-        delete caches[this.path]
-        resolve()
-      })
-    })
-  }
-
-  async destroy () {
-    logger.debug('destroy', this.path)
-
-    return new Promise((resolve, reject) => {
-      this._storage.destroy(this.path, (err) => {
-        if (err) {
-          return reject(err)
-        }
-        resolve()
-      })
-    })
+  constructor (store) {
+    this._store = store
   }
 
   async get (key) {
-    if (!this._store) {
-      await this.open()
-    }
-
-    if (this._store.status && this._store.status !== 'open') {
-      return Promise.resolve(null)
-    }
-
     return new Promise((resolve, reject) => {
       this._store.get(key, (err, value) => {
         if (err) {
@@ -91,15 +29,7 @@ class Cache {
   }
 
   // Set value in the cache and return the new value
-  async set (key, value) {
-    if (!this._store) {
-      await this.open()
-    }
-
-    if (this._store.status && this._store.status !== 'open') {
-      return Promise.resolve()
-    }
-
+  set (key, value) {
     return new Promise((resolve, reject) => {
       this._store.put(key, JSON.stringify(value), (err) => {
         if (err) {
@@ -114,12 +44,23 @@ class Cache {
     })
   }
 
+  load(directory, dbAddress, store) {
+    logger.debug('load, database:', dbAddress.toString())
+
+    return this;
+
+    // const dbPath = path.join(dbAddress.root, dbAddress.path)
+    // const dataPath = path.join(directory, dbPath)
+
+    // const cache = new Cache(store, dataPath)
+    // return cache
+  }
+
+  close() { } // noop
+  destroy() { } // noop
+
   // Remove a value and key from the cache
   async del (key) {
-    if (!this._store) {
-      await this.open()
-    }
-
     return new Promise((resolve, reject) => {
       this._store.del(key, (err) => {
         if (err) {
@@ -135,29 +76,4 @@ class Cache {
   }
 }
 
-module.exports = (storage, mkdir) => {
-  return {
-    load: async (directory, dbAddress) => {
-      logger.debug('load, database:', dbAddress.toString())
-
-      const dbPath = path.join(dbAddress.root, dbAddress.path)
-      const dataPath = path.join(directory, dbPath)
-      let cache = caches[dataPath]
-      if (!cache) {
-        if (mkdir && mkdir.sync) {
-          mkdir.sync(dataPath)
-        }
-        cache = new Cache(storage, dataPath)
-        await cache.open()
-        caches[dataPath] = cache
-      }
-      return cache
-    },
-    close: async () => {
-      logger.debug('close all')
-
-      await Promise.all(Object.values(caches), cache => cache.close())
-      caches = {}
-    }
-  }
-}
+module.exports = Cache
--- index-nodejs.js
+++ index-nodejs.js
@@ -1,4 +1,2 @@
-const level = require('leveldown')
-const mkdirp = require('mkdirp')
 const Cache = require('./Cache')
-module.exports = Cache(level, mkdirp)
+module.exports = Cache
